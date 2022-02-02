6b8182e56a2e07e01682d0d6cd1f454ecd956e9a
johngeorge142 <70117125+johngeorge142@users.noreply.github.com>
2022-02-01 20:44:23 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created')
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped')
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name='Mercury',
+                     planet_type='Class D',
+                     home_star = 'Sol',
+                     mass=3.258e23,
+                     radius=1516,
+                     distance=35.98e6)
+
+    venus = Planet(planet_name='Venus',
+                     planet_type='Class K',
+                     home_star = 'Sol',
+                     mass=4.258e23,
+                     radius=1516,
+                     distance=35.98e6)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+
+    test_user = User(first_name='John',
+                     last_name='George',
+                     email = 'johngeorge142@gmail.com',
+                     password = 'password')
+
+    db.session.add(test_user)
+    db.session.commit()
+    print('Database seeded')
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/super_simple')
+def super_simple():
+    return jsonify(message='Hello from the Planetary API')
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message="not found"), 404
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name') #request.args gives access to all URL variables
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message = "Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message = "Welcome " + name + ", you are old enough!"), 200
+
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message = "Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message = "Welcome " + name + ", you are old enough!"), 200
+
+
+@app.route('/planets', methods=['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
+
+
+@app.route('/planet_details/<int:planet_id>', methods=["GET"])
+def planet_details(planet_id: int):
+        planet = Planet.query.filter_by(planet_id=planet_id).first()
+        if planet:
+            result = planet.schema.dump(planet)
+            return jsonify(result.data)
+        else:
+            return jsonify(message="That planet doesnt exist"), 404
+            
+# database models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique = True)
+    password = Column(String)
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
+
+if __name__ == '__main__':
+    app.run()
+
